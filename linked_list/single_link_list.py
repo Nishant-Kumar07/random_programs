86abd6837e8b4db02c3c687a72ec731cd7971406
Nishant Kumar <nk613n@att.com>
2020-07-10 00:53:09 +0000

    [Single_link_list] Add a node at a specific position

--- linked_list/single_link_list.py
+++ linked_list/single_link_list.py
@@ -29,6 +29,28 @@ class SLinkedList:
              curr = curr.nextval
         return False
 
+    def add_node(self, pos, node):
+        # Add a node:
+        # pos 0, middle or last
+        # 1. if position first then make that as head node and existing head node becomes current's next node
+        # 2. If position in middle then make previous next node as current and current next node as next node
+        
+        pos_count = 0
+        prev = None
+        curr = self.headval
+        #import pdb; pdb.set_trace()
+        while curr is not None:
+            if pos_count == pos:
+                if prev:
+                    prev.nextval = node
+                    node.nextval = curr
+                else:
+                    self.headval = node
+                    node.nextval = curr
+                return True
+            pos_count+=1
+            prev = curr
+            curr = curr.nextval
 
 
 list = SLinkedList()
@@ -42,8 +64,23 @@ list.headval.nextval = e2
 # Link second Node to third node
 e2.nextval = e3
 
+
+#list.delete_node(e2)
+
+
+e4 = Node("Thurs")
+
+print("*********\n")
+
+list.add_node(0,e4)
 list.listprint()
 
-list.delete_node(e2)
+e5 = Node("Friday")
+print("*********\n")
+list.add_node(1,e5)
+list.listprint()
 
+e6 = Node("Saturday")
+print("*********\n")
+list.add_node(9,e6)
 list.listprint()
